d833d6ce016f0fc32c561e422a5b52b7c375ffcc
radarbogor <radarbogor.dev@gmail.com>
2023-09-13 15:56:22 +0700

    Update analitycs tag

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -14,14 +14,13 @@
     <!-- //Summernote CSS - CDN Link -->
 
     {{-- Google Analityc --}}
-    <script src="https://www.googletagmanager.com/gtag/js?id=UA-263169045-1" id="google_gtagjs-js" async type="719a15583f94f26009c4e225-text/javascript"></script>
-    <script id="google_gtagjs-js-after" type="719a15583f94f26009c4e225-text/javascript">
-    window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}
-    gtag('set', 'linker', {"domains":["radarbekasi.id"]} );
-    gtag("js", new Date());
-    gtag("set", "developer_id.dZTNiMT", true);
-    gtag("config", "UA-263169045-1", {"anonymize_ip":true});
-    gtag("config", "GT-M6PJLW3");
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-9TEZQFDETZ"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-9TEZQFDETZ');
     </script>
 
     {{-- Fancy Box --}}
